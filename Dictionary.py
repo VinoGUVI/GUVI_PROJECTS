3f8b0ad31b72342f71855eccf37faed1de06d958
VinoGUVI <vinodha@guvi.in>
2024-06-10 19:30:21 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,71 @@
+#OPERATORS - 7
+#ARITHMETIC
+"""a=10
+b=3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b) #floor
+print(a%b)  #Remainder"""
+"""
+#assignment operator
+a=10
+b=5
+a+=10 #a=a+10
+print(a)
+b-=2 #a=a-2
+print(b)
+a*=3
+print(a)
+b/=a
+print(b)
+b//=2
+print(b)
+a%=3
+print(a)
+a**=3
+print(a)
+"""
+"""#Comparison - >,<,>=,<=,==,!=
+a=int(input("Enter a number: "))
+b=int(input("Enter a number: "))
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+print(a==b)
+print(a!=b)"""
+
+"""#Logical operator - and ,or ,not
+a=34
+print(a>30 and a<34) #true and false
+print(a>30 or a<34) #true or false
+print(not(a>30))"""
+
+
+#Membership - in ,not in
+l=[2,3,4,"Hello"]
+print(2 in l)
+print(4 not in l)
+
+#Identity operator - is, is not (used to compare the 
+l=[1,2,3]
+l1=[1,2,3]
+print(l==l1)
+print(l is l1)
+print(l is not l1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
